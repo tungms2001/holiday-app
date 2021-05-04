a43ba1da1f8d1178e7297d0b159e9c0cef238095
= <=>
2021-05-04 20:06:01 +0700

    first commit

---
+++ app/src/main/java/com/example/holiday/TourInfoBlock.java
@@ -0,0 +1,14 @@
+package com.example.holiday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TourInfoBlock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tour_info_block);
+    }
+}
